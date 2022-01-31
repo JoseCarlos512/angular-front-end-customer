9655cd2e186a8cd433717384d4586a8301983cfc
JoseCarlos <jose.leon@deister.es>
2022-01-31 16:48:38 +0100

    start-proyect add client

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [{
+    id: 1,
+    nombre: 'Jose Carlos',
+    apellido: 'Leon Tito',
+    email: 'jose.leon@deister.es',
+    createAt: '2018-08-12'
+  }];
