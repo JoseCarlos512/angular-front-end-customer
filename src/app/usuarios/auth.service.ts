dfd24ebbbe1dbf482cdf5f5a9cd88e9530e632cb
Jose Leon <ljosecarlos295@gmail.com>
2022-02-27 19:13:02 -0500

    implementar clase servicio AuthService y guardar usuario y token en sessionStorage

---
+++ src/app/usuarios/auth.service.ts
@@ -0,0 +1,109 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Usuario } from './usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private usuario!: Usuario;
+  private token!: string | null;
+  /** 
+   * Agregarlo como parametro de entrada del constructor 
+   * es como decir que se declaro fuera y se esta inicializando
+   * dentro del contructor {} 
+   */
+  constructor(private http: HttpClient) { }
+
+  public getUsuario():Usuario {
+
+    const objUsuarioSession = sessionStorage.getItem('usuario');
+
+    if (this.usuario != null){
+      return this.usuario;
+    } else if (this.usuario == null && objUsuarioSession != null) {
+      this.usuario = JSON.parse(objUsuarioSession) as Usuario;
+      return this.usuario;
+    }
+    return new Usuario();
+  }
+
+  public getToken():string | null {
+    
+    const mObjTokenSession = sessionStorage.getItem('token');
+
+    if (this.token != null){
+      return this.token;
+    } else if (this.token == null && mObjTokenSession != null) {
+      this.token = mObjTokenSession;
+      return this.token;
+    }
+    return null;
+  }
+
+  login(usuario: Usuario): Observable<any> {
+    const urlEndpoint = 'http://localhost:8080/oauth/token';
+    
+    /**
+     * Credenciales de la aplicacion
+     */
+    const credenciales = btoa('angularapp' + ':' + '12345')
+    const httpHeaders = new HttpHeaders(
+                                        {
+                                          'Content-Type': 'application/x-www-form-urlencoded',
+                                          'Authorization': 'Basic ' + credenciales
+                                        }
+                                      ); 
+
+    /**
+     * Credenciales de usuario registrado en la bd
+     * la variable params que almacena los crenciales
+     * almacena la informacion en un tipo de urlencoded
+     * lo que falta para poder mandarla al endpoint es
+     * transformarlo a cadena con toString();
+     */
+    let params = new URLSearchParams();
+        params.set('grant_type', 'password');
+        params.set('username', usuario.username);
+        params.set('password', usuario.password);
+    
+        console.log(params.toString());
+    return this.http.post<any>(urlEndpoint, params.toString(), {headers: httpHeaders});
+  }
+
+  guardarUsuario(accessToken: string): void {
+    let mObjPayload = this.obtenerDatosToken(accessToken);
+
+    /**
+     * Almacenar la informacion recogida en el objeto usuario
+     */
+    this.usuario = new Usuario();
+    this.usuario.nombre   = mObjPayload.nombre;
+    this.usuario.apellido = mObjPayload.apellido;
+    this.usuario.email    = mObjPayload.email;
+    this.usuario.username = mObjPayload.user_name;
+    this.usuario.roles    = mObjPayload.authorities;
+    
+    // stringify: Transforma el objeto o json en json plano
+    sessionStorage.setItem('usuario', JSON.stringify(this.usuario))
+  }
+
+  guardarToken(accessToken: string): void {
+    this.token = accessToken;
+    
+    /**
+     * Almacenar el token en session
+     */
+    sessionStorage.setItem('token', this.token);
+  }
+
+  obtenerDatosToken(accessToken: string): any {
+    if (accessToken != null){
+      return JSON.parse(atob(accessToken.split(".")[1]))
+    }
+    return null;
+  }
+}
--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { AuthService } from './auth.service';
 import { Usuario } from './usuario';
 
 @Component({
@@ -12,7 +14,10 @@ export class LoginComponent implements OnInit {
   public titulo:string = 'Por favor Sign In!';
   public usuario: Usuario;
 
-  constructor() {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
     // Se puede hacer tbm donde se creo, da los mismo
     // mas elegante en el constructor 
     this.usuario = new Usuario();
@@ -28,6 +33,27 @@ export class LoginComponent implements OnInit {
       Swal.fire("Error", "Username o Password vacias", "error");
       return;
     }
+
+    this.authService.login(this.usuario).subscribe(
+      response => {
+        console.log(response);
+
+        /**
+         * Guardar el token y el usuario
+         */
+        this.authService.guardarUsuario(response.access_token);
+        this.authService.guardarToken(response.access_token);
+
+        let usuario = this.authService.getUsuario();
+
+        this.router.navigate(['/clientes']);
+        Swal.fire('Login', 'Hola ' + usuario.username + ' has iniciado sesion con exito', 'success')
+      }, err => {
+        if (err.status == 400) {
+          Swal.fire('Error login', 'Usuario o clave incorrecta!', 'error')
+        }
+      }
+    );
   }
 
 }
