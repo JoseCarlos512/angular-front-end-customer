657fb1fb2957f562c18cbaedfb0325e2939b9ce9
Jose Leon <ljosecarlos295@gmail.com>
2022-04-03 05:09:13 -0500

    arreglar detalles en la linea de factura, no tener productos duplicados en la factura, agregar delete y calcular total dianmicamente

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -20,7 +20,6 @@ export class FacturasComponent implements OnInit {
   factura: Factura = new Factura();
 
   autocompleteControl = new FormControl();
-  productos: string[] = ['Horno', 'Mesa', 'Silla'];
   productosFiltrados!: Observable<Producto[]>;
   
   constructor(
@@ -60,18 +59,26 @@ export class FacturasComponent implements OnInit {
     let producto = event.option.value as Producto;
     console.log(producto);
 
-    let nuevoItem = new ItemFactura();
-    nuevoItem.producto = producto;
-
-    this.factura.items.push(nuevoItem);
+    if (this.existeItem(producto.id)) {
+      this.incrementaCantidad(producto.id);
+    } else {
+      let nuevoItem = new ItemFactura();
+      nuevoItem.producto = producto;
+      this.factura.items.push(nuevoItem);
+    }
 
     this.autocompleteControl.setValue('');
     event.option.focus();
     event.option.deselect();
+
   }
 
   actualizarCantidad(id:number, event:any): void {
     let cantidad:number = event.target.value as number;
+
+    if (cantidad == 0) {
+      return this.eliminarItemFactura(id);
+    }
     
     this.factura.items = this.factura.items.map((item:ItemFactura) => {
       if (id === item.producto.id) {
@@ -80,5 +87,29 @@ export class FacturasComponent implements OnInit {
       return item;
     })
   }
+
+  existeItem(id:number):boolean {
+    let existe = false;
+    this.factura.items.forEach((item:ItemFactura) => {
+      if (id === item.producto.id) {
+        existe = true;
+      }
+    })
+    return existe;
+  }
+
+  incrementaCantidad(id:number):void {
+    this.factura.items = this.factura.items.map((item:ItemFactura) => {
+      if (id === item.producto.id) {
+        ++item.cantidad
+      }
+      return item;
+    })
+  }
+
+  eliminarItemFactura(id:number):void{
+    this.factura.items = this.factura.items.filter((item:ItemFactura) => id !== item.producto.id)
+  }
+
 
 }
